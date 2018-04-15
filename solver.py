0b8c890501d9fda51ab7cac39d2890227d7ddf8f
Danilo Fuchs <danilo_fuchs@hotmail.com>
2018-04-15 16:00:41 -0300

    Retrodistribuicao (incompleta) e LU (incompleto

--- solver.py
+++ solver.py
@@ -6,10 +6,33 @@ def sumLines(lineA: np.ndarray, lineB: np.ndarray):
 def multLine(line: np.ndarray, mult):
     return line.__mul__(mult)
 
+def swapLines(matrix: np.ndarray, lineindex1, lineIndex2) :
+    matrix[[lineindex1,lineIndex2]] = matrix[[lineIndex2,lineindex1]]
+    return matrix
+
+def retroDistribution(matrix: np.ndarray) :
+    nVar = matrix.shape[0]
+
+    for i in range(nVar) : #para cada linha
+        numCoefsLine = 0
+        lastCoef = (-1,-1)
+        for j in range(nVar) : #para cada item da linha
+            if matrix[i][j] != 0 : #se acha um coeficiente != 0
+                numCoefsLine += 1
+                lastCoef = (i,j)
+        if numCoefsLine == 1 : #se só tem 1 coeficiente na linha
+            multLine(matrix[i], (1/matrix[lastCoef[0]][lastCoef[1]]))
+            for x in range (nVar) :
+                if x != i :
+                    matrix[x] = sumLines(matrix[x], multLine(matrix[i], -matrix[x][lastCoef[1]]))
+
+
+    return np.array(matrix[:, nVar])
+
 def gaussElimination(matrix: np.ndarray, nDigits):
     #results = 1
     #matrix = np.array(matrix)
-    nVar = matrix.shape[0]
+    nVar = matrix.shape[0] #numero de linhas da matriz
     for j in range((nVar - 1), 0, -1): #das colunas (nVar-1) ate 0
         for i in range(nVar): #das linhas 0 ate nVar
             mult = matrix[i][j] / matrix[j][j] # mult = matriz[nvar-1][]/matriz
@@ -17,4 +40,19 @@ def gaussElimination(matrix: np.ndarray, nDigits):
             if (nDigits >= 0) :
                 matrix.round(nDigits)
             print(matrix)
-    return [row[nVar] for row in matrix]
+    return retroDistribution(matrix)
+
+def luDecomposition(matrix: np.ndarray, nDigits) :
+    nVar = matrix.shape[0] #numero de linhas da matriz
+    coefMatrix = np.delete(matrix, nVar, 1)
+    bMatrix = np.array(matrix[:, nVar])
+
+    
+
+    print(coefMatrix)
+    print(bMatrix)
+
+    X = np.array()
+    Y = np.array()
+
+    return X
